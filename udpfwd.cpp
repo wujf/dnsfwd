f3c68a81c13219f37f662abb8258d2476385509c
Jianfeng Wu <jianfeng.wu@me.com>
2015-10-27 13:49:46 +0800

    Fix minor issues

--- udpfwd.cpp
+++ udpfwd.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <cerrno>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -53,13 +54,13 @@ int init_ss() {
 }
 
 int udp_via_socks(char *data, size_t size) {
-#if 1
+#if 0
     int ss_sock = init_ss();
     // Greetings
     const char greeting[] = { 0x05, 0x01, 0x00 };
     ::send(ss_sock, greeting, 3, 0);
 
-    char res[1000];
+    unsigned char res[1000];
     ssize_t rc;
     rc = ::recv(ss_sock, res, sizeof(res), 0);
     if (rc < 0) {
@@ -69,10 +70,8 @@ int udp_via_socks(char *data, size_t size) {
     }
 
     printf("UDP associate res size: %ld: ", rc);
-    for (int i = 0; i < rc; i++) {
+    for (int i = 0; i < rc; i++)
         printf("%02x ", res[i]);
-    }
-
     printf("\n");
 
     // UDP assoc
@@ -109,6 +108,8 @@ int udp_via_socks(char *data, size_t size) {
 #endif
 
     // send the real payload
+    auto dns_addr = ::inet_addr("8.8.8.8");
+    auto dns_port = htons(53);
     data[0] = data[1] = '\x00';   // Reserved
     data[2] = 0;                 // Frag
     data[3] = '\x01';            // IPv4
@@ -132,11 +133,11 @@ int udp_via_socks(char *data, size_t size) {
         return -1;
     }
 
-    printf("UDP payload sent: %ld\n", sent);
-    printf("==================\n");
+    //printf("UDP payload sent: %ld\n", sent);
     return data_sock;
 }
 
+#if 0
 int plain_forward(void *data, size_t size) {
     // parse/validate request
 
@@ -161,6 +162,7 @@ int plain_forward(void *data, size_t size) {
     printf("DNS request sent: %ld\n", rc);
     return dns_sock;
 }
+#endif
 
 int run(u_short port) {
     // listen sock
@@ -198,10 +200,11 @@ int run(u_short port) {
             break;
         }
 
+        printf("========= poll=%d =========\n", rc);
         // request
         if (fds[0].revents) {
             ::sockaddr_in from;
-            ::socklen_t addrlen;
+            ::socklen_t addrlen = sizeof(from);
             char* ptr = buf + 10;
             size_t limit = buflen - 10;
             ssize_t rsize = ::recvfrom(listen_sock, ptr, limit, 0,
@@ -212,7 +215,9 @@ int run(u_short port) {
                 break;
             }
 
-            std::cout << "Got " << rsize << " bytes from client: " << ntohs(from.sin_port) << std::endl;
+            std::cout << "Got " << rsize << " bytes from client: "
+                      << ntohs(from.sin_port) << "/" << addrlen <<  std::endl;
+
             char hostname[256];
             parse_dns(ptr + 12, hostname, sizeof(hostname)); 
             printf("dns: [%s]\n", hostname);
@@ -240,17 +245,28 @@ int run(u_short port) {
                     break;
                 }
         
-                std::cout << "Got " << rsize << " bytes from server" << std::endl;
                 auto client = gClients[sock];
+                char ip[20];
+                ::inet_ntop(AF_INET, &client->sin_addr, ip, 20);
+                std::cout << "Got " << rsize << " bytes for client "
+                          << ip << ":" << ntohs(client->sin_port) << std::endl;
+#if 0
+                for (int i = 0; i < rsize; i++)
+                    printf("%02x ", (uint8_t)buf[i]);
+                printf("\n");
+#endif
+
                 ssize_t sent;
-                sent = ::sendto(listen_sock, buf, rsize, 0,
+                sent = ::sendto(listen_sock, buf + 10, rsize - 10, 0,
                                 reinterpret_cast<sockaddr*>(client),
                                 sizeof(*client));
-                if (sent != rsize) {
-                    ::perror("send to client");
+                if (sent != rsize - 10) {
+                    std::cerr << "fwd to client " << listen_sock << ": "
+                              << strerror(errno) << std::endl; 
                     break;
                 }
 
+                std::cout << "Reply fwd'd: " << sent << std::endl;
                 gClients.erase(sock);
                 delete client;
                 ::close(sock);
@@ -265,6 +281,10 @@ int run(u_short port) {
 
 }       // namespace
 
-int main() {
-    return UdpForwarder::run(53);
+int main(int argc, char* argv[]) {
+    u_short port = 1953;
+    if (argc > 1)
+        port = atoi(argv[1]);
+
+    return UdpForwarder::run(port);
 }
